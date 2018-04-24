29c298e22355d9e2463336d71dfd4f9845884f6d
PLBegay <pierreleo.begay@gmail.com>
2018-04-24 14:22:26 +0200

    Added int8 type

--- octant/datalog_primitives.py
+++ octant/datalog_primitives.py
@@ -156,6 +156,7 @@ TYPES = {
     'id': StringType('id'),
     'int': NumType('int'),
     'int4': NumType('int', size=4),
+    'int8': NumType('int', size=8),
     'int16': NumType('int', size=16),
     'direction': StringType('direction', size=2),
     'status': StringType('status', size=3),
